ed2c758f73af4a3c51d2cf4e561c1790efbbc683
Charlie <jnr.31gcharlieedokpa@gmail.com>
2023-02-15 12:49:58 +0100

    15/02/2023 modificado prueba

--- src/alumnos/AñadirAlumnos.java
+++ src/alumnos/AñadirAlumnos.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 public class AñadirAlumnos {
 
     /**
-     * Modificado
+     * Modificado fdsf
      * @param alum Sera la tabla de alumnos
      * @param i Sera el indice para añadir los alumnos a la tabla
      * @param entrada Sera para escribir en teclado;
